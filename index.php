d47d232c4ef7bed5534b5be4d5030828fcf7f382
joker <joker_xii@outlook.com>
2018-05-30 20:36:14 +0800

    fixing redirct

--- index.php
+++ index.php
@@ -8,20 +8,23 @@
 $cur_select=$_SERVER['REQUEST_URI'];
 $cur_root='/algo/';
 $pack=array_filter(glob('*'), 'is_dir');
+//var_dump($pack);
 $cur_select=str_replace($cur_root,'',$cur_select);
 $cur_select=str_replace('/','',$cur_select);
-$exists=false;
+//$exists=false;
 if(!strcmp($cur_select,'')){
     include 'list.html';
     echo "<script>addDirs(".json_encode($pack) .")</script>";
-    $exists=true;
-} else foreach ($pack as $val){
-    if(!strcmp($val,$cur_select)){
-        $exists=true;
-        include $val.'/index.html';
-    }
+//    $exists=true;
 }
-if(!$exists){
+//else foreach ($pack as $val){
+//    if(!strcmp($val,$cur_select)){
+//        $exists=true;
+//        echo "<script>console.log('calling')</script>";
+//        include $val.'/index.html';
+//    }
+//}
+else{
     header('location:'.$cur_root);
 }
 
