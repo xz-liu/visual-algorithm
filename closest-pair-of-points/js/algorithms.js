c4e5520f26588e00783bf39f51b117e994331a55
joker <joker_xii@outlook.com>
2018-06-01 17:55:48 +0800

    fix algo

--- closest-pair-of-points/js/algorithms.js
+++ closest-pair-of-points/js/algorithms.js
@@ -1,185 +1,139 @@
-var disCalledTimes=0;
+var disCalledTimes = 0;
 function resetCalledTimes() {
-    disCalledTimes=0;
+    disCalledTimes = 0;
 }
 
 function getCalledTimes() {
     return disCalledTimes;
 }
 
-function distance(p1,p2) {
+function distance(p1, p2) {
     disCalledTimes++;
-    var x=(p1[0]-p2[0]);
-    var y=(p1[1]-p2[1]);
-    return Math.sqrt(x*x+y*y);
+    var x = (p1[0] - p2[0]);
+    var y = (p1[1] - p2[1]);
+    return Math.sqrt(x * x + y * y);
 }
 
 function bruteForce(points) {
-    var minDis,minPair;
-    for(var i=0;i<points.length;i++){
-        for(var j=i+1; j<points.length;j++){
-                if (minDis === undefined) {
-                    minDis = distance(points[i],points[j]);
-                    minPair=[i,j];
-                }else {
-                    var now=distance(points[i],points[j]);
-                    if(now<minDis){
-                        minDis = now;//distance(points[i],points[j]);
-                        minPair=[i,j];
-                    }
+    var minDis, minPair;
+    for (var i = 0; i < points.length; i++) {
+        for (var j = i + 1; j < points.length; j++) {
+            if (minDis === undefined) {
+                minDis = distance(points[i], points[j]);
+                minPair = [i, j];
+            } else {
+                var now = distance(points[i], points[j]);
+                if (now < minDis) {
+                    minDis = now;//distance(points[i],points[j]);
+                    minPair = [i, j];
                 }
+            }
         }
     }
     return [minDis].concat(minPair);
 }
 // v
-function bruteForceDemo(points,line) {
-    var minDis,minPair;
-    for(var i=0;i<points.length;i++){
-        for(var j=i+1; j<points.length;j++){
-                if (minDis === undefined) {
-                    minDis = distance(points[i],points[j]);
-                    minPair=[i,j];
+function bruteForceDemo(points, line) {
+    var minDis, minPair;
+    for (var i = 0; i < points.length; i++) {
+        for (var j = i + 1; j < points.length; j++) {
+            if (minDis === undefined) {
+                minDis = distance(points[i], points[j]);
+                minPair = [i, j];
+                line.push([minDis].concat(minPair.concat(['red'])));
+            } else {
+                var now = distance(points[i], points[j]);
+                line.push([now, i, j, 'blue']);
+                if (now < minDis) {
+                    minDis = now; //distance(points[i],points[j]);
+                    minPair = [i, j];
                     line.push([minDis].concat(minPair.concat(['red'])));
-                }else {
-                    var now=distance(points[i],points[j]);
-                    line.push([now,i,j,'blue']);
-                    if(now<minDis){
-                        minDis = now; //distance(points[i],points[j]);
-                        minPair=[i,j];
-                        line.push([minDis].concat(minPair.concat(['red'])));
-                    }
                 }
+            }
         }
     }
     line.push([minDis].concat(minPair.concat(['green'])));
     return [minDis].concat(minPair);
 }
-function cmpByY(p1,p2) {
-    return Math.sign(p2[1]-p1[1]);
+function cmpByY(p1, p2) {
+    return Math.sign(p2[1] - p1[1]);
 }
-function cmpByY_sortT(p1,p2) {
-    return cmpByY(p1[0],p2[0]);
+function cmpByY_sortT(p1, p2) {
+    return cmpByY(p1[0], p2[0]);
 }
 
 function algo(points) {
     points.sort(cmpByY);
-    // console.log(0+' '+(parseInt(points.length)-1));
-    return closest(points,0,points.length-1);
+    return closest(points, 0, points.length - 1);
 }
 
-function closest(p,l,r) {
-    // console.log('cnt'+l+" "+r);
-    if(l===r){
-        // console.log('base1 '+l+" "+r);
-        return [Number.MAX_VALUE,l,r];
+function closest(p, l, r) {
+    if (l === r) {
+        return [Number.MAX_VALUE, l, r];
     }
-    if((l+1)===r){
-        // console.log('base2 '+l+" "+r);
-        return [distance(p[l],p[r]),l,r];
+    if ((l + 1) === r) {
+        return [distance(p[l], p[r]), l, r];
     }
-    if((l+2)===r) {
-        var traid = [[distance(p[l], p[r]), l, r],
-            [distance(p[l], p[l + 1]), l, l + 1],
-            [distance(p[l + 1], p[r]), l + 1, r]];
-        var ret=0;
-        for (var i in traid){
-            if(traid[i][0]<traid[ret][0]){
-                ret=i;
-            }
-        }
-        return traid[ret];
-    }
-    var mid=parseInt((r+l)/2);
-    var dis1=closest(p,l,mid);
-    var dis2=closest(p,mid+1,r);
-    var tr=dis2;
-    if(dis1[0]<dis2[0]){tr=dis1;}
-    // else {tr=dis2;}
-    var t=[];
-    for(i=l;i<=r;i++){
-        if(Math.abs(p[i][0]-p[mid][0])<=tr[0]){
-            t.push([p[i],i]);
-        }
-        else if(Math.abs(p[i][0]-p[mid+1][0])<=tr[0]){
-            t.push([p[i],i]);
+    var mid = parseInt((r + l) / 2);
+    var dis1 = closest(p, l, mid);
+    var dis2 = closest(p, mid + 1, r);
+    var tr = dis2;
+    if (dis1[0] < dis2[0]) { tr = dis1; }
+    var t = [];
+    for (i = l; i <= r; i++) {
+        if (Math.abs(p[i][0] - p[mid][0]) <= tr[0]) {
+            t.push([p[i], i]);
         }
     }
     t.sort(cmpByY_sortT);
-    for(i=0;i<t.length;i++){
-        for(var j=i+1;j<t.length&&(t[j][0][1]-t[i][0][1])<tr[0];j++){
-            var tmpDis=distance(t[i][0],t[j][0]);
-            if(tmpDis<tr[0]){
-                tr=[tmpDis,t[i][1],t[j][1]];
+    for (i = 0; i < t.length; i++) {
+        for (var j = i + 1; j < t.length && (t[j][0][1] - t[i][0][1]) < tr[0]; j++) {
+            var tmpDis = distance(t[i][0], t[j][0]);
+            if (tmpDis < tr[0]) {
+                tr = [tmpDis, t[i][1], t[j][1]];
             }
         }
     }
-    // console.log('RET!!!!!!!!');
-    // console.log(tr);
     return tr;
 }
 
 
-function algoDemo(points,lines) {
+function algoDemo(points, lines) {
     points.sort(cmpByY);
-    // console.log(0+' '+(parseInt(points.length)-1));
-    var res= closestDemo(points,0,parseInt(points.length)-1,lines);
+    var res = closestDemo(points, 0, parseInt(points.length) - 1, lines);
     lines.push(res.concat('green'));
     return res;
 }
 
 function closestDemo(p, l, r, lines) {
-    // console.log('cnt'+l+" "+r);
-    if(l===r){
-        // console.log('base1 '+l+" "+r);
-        return [Number.MAX_VALUE,l,r];
+    if (l === r) {
+        return [Number.MAX_VALUE, l, r];
     }
-    if((l+1)===r){
-        // console.log('base2 '+l+" "+r);
-        return [distance(p[l],p[r]),l,r];
+    if ((l + 1) === r) {
+        return [distance(p[l], p[r]), l, r];
     }
-
-    if((l+2)===r) {
-        var traid = [[distance(p[l], p[r]), l, r],
-            [distance(p[l], p[l + 1]), l, l + 1],
-            [distance(p[l + 1], p[r]), l + 1, r]];
-        var ret=0;
-        for (var i in traid){
-            if(traid[i][0]<traid[ret][0]){
-                ret=i;
-            }
-        }
-        return traid[ret];
-    }
-    var mid=parseInt((r+l)/2);
-    var dis1=closestDemo(p,l,mid,lines);
-    var dis2=closestDemo(p,mid+1,r,lines);
-    lines.push(dis1.concat('blue'));lines.push(dis2.concat('blue'));
-    var tr=dis2;
-    if(dis1[0]<dis2[0]){tr=dis1;}
-    // else {tr=dis2;}
-    var t=[];
-    for(i=l;i<=r;i++){
-        if(Math.abs(p[i][0]-p[mid][0])<=tr[0]){
-            t.push([p[i],i]);
-        }
-        else if(Math.abs(p[i][0]-p[mid+1][0])<=tr[0]){
-            t.push([p[i],i]);
+    var mid = parseInt((r + l) / 2);
+    var dis1 = closestDemo(p, l, mid, lines);
+    var dis2 = closestDemo(p, mid + 1, r, lines);
+    lines.push(dis1.concat('blue')); lines.push(dis2.concat('blue'));
+    var tr = dis2;
+    if (dis1[0] < dis2[0]) { tr = dis1; }
+    var t = [];
+    for (i = l; i <= r; i++) {
+        if (Math.abs(p[i][0] - p[mid][0]) <= tr[0]) {
+            t.push([p[i], i]);
         }
     }
     t.sort(cmpByY_sortT);
-    for(i=0;i<t.length;i++){
-        for(var j=i+1;j<t.length;j++){
-            if((t[j][0][1]-t[i][0][1])>tr[0])continue;
-            var tmpDis=distance(t[i][0],t[j][0]);
-            if(tmpDis<tr[0]){
-                tr=[tmpDis,t[i][1],t[j][1]];
+    for (i = 0; i < t.length; i++) {
+        for (var j = i + 1; j < t.length && (t[j][0][1] - t[i][0][1]) < tr[0]; j++) {
+            var tmpDis = distance(t[i][0], t[j][0]);
+            if (tmpDis < tr[0]) {
+                tr = [tmpDis, t[i][1], t[j][1]];
                 lines.push(tr.concat('blue'));
             }
         }
     }
-    // console.log('RET!!!!!!!!');
-    // console.log(tr);
     lines.push(tr.concat('red'));
     return tr;
 }
--- closest-pair-of-points/js/scripts.js
+++ closest-pair-of-points/js/scripts.js
@@ -1,161 +1,177 @@
 var gridNow;
 var lineHorizontal;
 var lineVertical;
-var showIndex=0;
-$(window).mousemove(function( event ) {
+var showIndex = 0;
+$(window).mousemove(function (event) {
 
-    if(gridNow){
-        var p=$('#output_grid').offset();
-        var r=gridNow.getCoordinate(event.pageX-p.left,
-                event.pageY-p.top);
-        lineVertical.css("top",event.clientY);
-        lineVertical.css("left",0);
-        lineHorizontal.css("top",0);
-        lineHorizontal.css("left",event.clientX);
-        var posNow=$('#pos_now');
-        posNow.html("("+parseInt(r[0])+","
-            +parseInt(r[1])+")");
-        posNow.css("left",event.clientX);
-        posNow.css("top",event.clientY);
+    if (gridNow) {
+        var p = $('#output_grid').offset();
+        var r = gridNow.getCoordinate(event.pageX - p.left,
+            event.pageY - p.top);
+        lineVertical.css("top", event.clientY);
+        lineVertical.css("left", 0);
+        lineHorizontal.css("top", 0);
+        lineHorizontal.css("left", event.clientX);
+        var posNow = $('#pos_now');
+        posNow.html("(" + parseInt(r[0]) + ","
+            + parseInt(r[1]) + ")");
+        posNow.css("left", event.clientX);
+        posNow.css("top", event.clientY);
     }
     // console.log(event.clientX+""+event.clientY);
 });
 
 
 function updateCanvas() {
-    document.getElementById('output_grid').width = window.innerWidth*0.98;
+    document.getElementById('output_grid').width = window.innerWidth * 0.98;
     console.log('update width');
-    if(gridNow)gridNow.act();
+    if (gridNow) gridNow.act();
 }
 
 $("#generate").click(function () {
-   var xMin=$('#x_min').val(),
-       xMax=$('#x_max').val(),
-       yMin=$('#y_min').val(),
-       yMax=$('#y_max').val();
-   var cnt=$('#points_count').val();
-   gridNow=new Grid('output_grid',xMin,xMax,yMax,yMin);
-   for (var i =0;i<cnt;i++){
-       var addNow=[Math.random()*(xMax-xMin)+xMin,
-                 Math.random()*(yMax-yMin)+yMin];
-       // console.log(addNow);
-       gridNow.addPoint(addNow[0],addNow[1]);
-   }
-   gridNow.act();
+    var xMin = $('#x_min').val(),
+        xMax = $('#x_max').val(),
+        yMin = $('#y_min').val(),
+        yMax = $('#y_max').val();
+    var cnt = $('#points_count').val();
+    gridNow = new Grid('output_grid', xMin, xMax, yMax, yMin);
+    for (var i = 0; i < cnt; i++) {
+        var addNow = [Math.random() * (xMax - xMin) + xMin,
+        Math.random() * (yMax - yMin) + yMin];
+        // console.log(addNow);
+        gridNow.addPoint(addNow[0], addNow[1]);
+    }
+    gridNow.act();
     $('#hint').show();
 });
 
-function getFormat(res, date1,date2, dis) {
-    var time=((date2-date1)/1000).toFixed(3);
-    return "Result:"+ res[0].toFixed(6)+
-        "<br>Point 1:("+parseFloat(res[1][0]).toFixed(3)+","+parseFloat(res[1][1]).toFixed(3)+")" +
-        "<br>Point 2:("+parseFloat(res[2][0]).toFixed(3)+","+parseFloat(res[2][1]).toFixed(3)+")" +
-        // "<br>"+
-        "<br>Time:"+time+"s" +
-        "<br>distance called "+dis+" times" +
-        "<br>";
+function getFormat(res, date1, date2, dis) {
+    var time = ((date2 - date1) / 1000).toFixed(3);
+    //     <div class="input-group ">
+    //     <div class="input-group-prepend">
+    //         <span class="input-group-text">X max</span>
+    //     </div>
+    //     <input type="number" class="form-control" readonly></input>
+    // </div>
+    var pre = '<div class="input-group "><div class="input-group-prepend"><span class="input-group-text">';
+    var mid = '</span></div><input type="text" class="form-control" readonly value="';
+    var end = '"></input></div>';
+    var show = '';
+    show += pre + 'RESULT' + mid + res[0].toFixed(6) + end;
+    show += pre + 'Point 1' + mid + "(" + parseFloat(res[1][0]).toFixed(3) + "," + parseFloat(res[1][1]).toFixed(3) + ")" + end;
+    show += pre + 'Point 2' + mid + "(" + parseFloat(res[2][0]).toFixed(3) + "," + parseFloat(res[2][1]).toFixed(3) + ")" + end;
+    show += pre + 'Time' + mid + time + 's' + end;
+    show += pre + 'distance() called' + mid + dis + " times" + end;
+    // return "Result:" ++
+    // "<br>Point 1:(" + parseFloat(res[1][0]).toFixed(3) + "," + parseFloat(res[1][1]).toFixed(3) + ")" +
+    // "<br>Point 2:(" + parseFloat(res[2][0]).toFixed(3) + "," + parseFloat(res[2][1]).toFixed(3) + ")" +
+    // // "<br>"+
+    // "<br>Time:" + time + "s" +
+    // "<br>distance called " + dis + " times" +
+    // "<br>";
+    return show;
 }
 
 function setResPoints(res, points) {
-    res[1]=points[res[1]];res[2]=points[res[2]];
+    res[1] = points[res[1]]; res[2] = points[res[2]];
     return res;
 }
 
 $("#run").click(function () {
     console.log('hi');
-    if(gridNow){
+    if (gridNow) {
         gridNow.clearLines();
         console.log('hello');
-        var points=gridNow.getPoints();
-        var date1=new Date();
+        var points = gridNow.getPoints();
+        var date1 = new Date();
         resetCalledTimes();
-        var res=bruteForce(points);
-        var dis1=getCalledTimes();
+        var res = bruteForce(points);
+        var dis1 = getCalledTimes();
         console.log(res);
-        var date2=new Date();
-       res= setResPoints(res,points);
-        var date3=new Date();
+        var date2 = new Date();
+        res = setResPoints(res, points);
+        var date3 = new Date();
         resetCalledTimes();
-        var res2=algo(points);
-        var dis2=getCalledTimes();
+        var res2 = algo(points);
+        var dis2 = getCalledTimes();
         console.log(res2);
-        var date4=new Date();
+        var date4 = new Date();
         // res2[1]=points[res2[1]];res2[2]=points[res2[2]];
-        res2=setResPoints(res2,points);
+        res2 = setResPoints(res2, points);
         console.log(res);
         console.log(res2);
-        var show="Brute force (O(n^2)):<br>"+
-            getFormat(res,date1,date2,dis1);
+        var show = "<h3>Brute force (O(n^2)):</h3>" +
+            getFormat(res, date1, date2, dis1);
 
-        var show2="Divide and Conquer (O(nlog(n))):<br>"+
-            getFormat(res2,date3,date4,dis2);
+        var show2 = "<h3>Divide and Conquer (O(nlog(n))):</h3>" +
+            getFormat(res2, date3, date4, dis2);
         console.log(show);
-        $('#result_text').html(show+show2);
-        gridNow.addVisLine(res[1],res[2],'blue');
-        gridNow.addVisLine(res2[1],res2[2],'yellow');
+        $('#result_text').html(show + show2);
+        gridNow.addVisLine(res[1], res[2], 'blue');
+        gridNow.addVisLine(res2[1], res2[2], 'yellow');
         $('#show_result').modal('show');
     }
 });
 
 $("#clear").click(function () {
-   if(gridNow){
-       gridNow.clear();
-       gridNow=undefined;
-       $('#hint').hide();
-   }
+    if (gridNow) {
+        gridNow.clear();
+        gridNow = undefined;
+        $('#hint').hide();
+    }
 });
 
-function showSteps(grid,points,lines,index,sIndex) {
-    var interval=100;
-    if(sIndex!==showIndex)return;
-    if(grid!==gridNow)return;
-    if(index>=lines.length)return;
+function showSteps(grid, points, lines, index, sIndex) {
+    var interval = 100;
+    if (sIndex !== showIndex) return;
+    if (grid !== gridNow) return;
+    if (index >= lines.length) return;
     // console.log(lines[index]);
-    gridNow.addVisLine(points[lines[index][1]],points[lines[index][2]],lines[index][3]);
+    gridNow.addVisLine(points[lines[index][1]], points[lines[index][2]], lines[index][3]);
     setTimeout(function () {
-        showSteps(grid,points,lines,index+1,sIndex);
-    },interval);
+        showSteps(grid, points, lines, index + 1, sIndex);
+    }, interval);
 }
 
 $("#run_demo").click(function () {
-    if(gridNow){
-    gridNow.clearLines();
-    var lines=[];
-    var points=gridNow.getPoints();
-    resetCalledTimes();
-    var date1= new Date();
-    var res=algoDemo(points,lines);
-    var date2= new Date();
-    // lines.push(res.concat('green'));
-    showSteps(gridNow,points,lines,0,++showIndex);
-    res=setResPoints(res,points);
-    // console.log(res);
-    $('#result_text').html(getFormat(res,date1,date2,getCalledTimes()));
-    $('#show_result').modal('show');
-}
+    if (gridNow) {
+        gridNow.clearLines();
+        var lines = [];
+        var points = gridNow.getPoints();
+        resetCalledTimes();
+        var date1 = new Date();
+        var res = algoDemo(points, lines);
+        var date2 = new Date();
+        // lines.push(res.concat('green'));
+        showSteps(gridNow, points, lines, 0, ++showIndex);
+        res = setResPoints(res, points);
+        // console.log(res);
+        $('#result_text').html(getFormat(res, date1, date2, getCalledTimes()));
+        $('#show_result').modal('show');
+    }
 });
 
 
 $('#run_bf_demo').click(function () {
-    if(gridNow){
+    if (gridNow) {
         gridNow.clearLines();
-        var lines=[];
-        var points=gridNow.getPoints();
+        var lines = [];
+        var points = gridNow.getPoints();
         resetCalledTimes();
-        var date1=new Date();
-        var res=bruteForceDemo(points,lines);
-        var date2=new Date();
+        var date1 = new Date();
+        var res = bruteForceDemo(points, lines);
+        var date2 = new Date();
         // lines.push(res.concat('green'));
-        res=setResPoints(res,points);
-        showSteps(gridNow,points,lines,0,++showIndex);
+        res = setResPoints(res, points);
+        showSteps(gridNow, points, lines, 0, ++showIndex);
         // console.log(res);
-        $('#result_text').html(getFormat(res,date1,date2,getCalledTimes()));
+        $('#result_text').html(getFormat(res, date1, date2, getCalledTimes()));
         $('#show_result').modal('show');
     }
 });
 $(document).ready(function () {
-    lineHorizontal=$('#line_horizontal');
-    lineVertical=$('#line_vertical');
+    lineHorizontal = $('#line_horizontal');
+    lineVertical = $('#line_vertical');
     updateCanvas();
     $('#x_min').val(1);
     $('#x_max').val(12345);
@@ -166,6 +182,6 @@ $(document).ready(function () {
 
 });
 
-$(window).on('resize',function () {
-   updateCanvas();
+$(window).on('resize', function () {
+    updateCanvas();
 });
